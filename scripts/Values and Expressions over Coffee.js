21ed6dc6eeb06aa5c3933cd1bd3f18176dfb2e46
Nazim Boudeffa <boudeffa@gmail.com>
2020-08-09 16:19:56 +0200

    Added chapters scripts

---
+++ scripts/Values and Expressions over Coffee.js
@@ -0,0 +1,36 @@
+42
+  //=> 42
+
+console.log("JavaScript" + " " + "Allonge");
+  //=> "JavaScript Allonge"
+
+2 === 2
+  //=> true
+
+'hello' !== 'goodbye'
+  //=> true
+
+2 === '2'
+  //=> false
+
+true !== 'true'
+  //=> true
+
+true === false
+  //=> false
+
+2 !== 5
+  //=> true
+
+'two' === 'five'
+  //=> false
+
+2 + 2 === 4
+  //=> true
+
+(2 + 2 === 4) === (2 !== 5)
+  //=> true
+
+[2-1, 2, 2+1] === [1,2,3]
+[1,2,3] === [1, 2, 3]
+[1, 2, 3] === [1, 2, 3]
